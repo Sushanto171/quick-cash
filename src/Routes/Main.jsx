88f0b088033bf9b00a14c1c4b0a5fca34823eead
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-02-24 16:48:04 +0600

    add home page

--- src/Routes/Main.jsx
+++ src/Routes/Main.jsx
@@ -1,9 +1,18 @@
 import { Outlet } from "react-router";
+import Container from "../components/share/Container";
+import Footer from "../components/share/Footer";
+import Navbar from "../components/share/Navbar";
 
 const Main = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="min-h-screen flex flex-col justify-between">
+      <Navbar />
+      <div className="flex-1">
+        <Container>
+          <Outlet />
+        </Container>
+      </div>
+      <Footer />
     </div>
   );
 };
